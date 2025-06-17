26ca1a2973b7a1e4f417fe6d402319d78c42d926
shubhammgits <shubhamm18.work@gmail.com>
2025-06-18 01:55:29 +0530

    update

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
 const video1 = document.getElementById('projectvideo1');
 const video2 = document.getElementById('projectvideo2');
 const video3 = document.getElementById('projectvideo3');
+const hoverSign = document.querySelector('.hover-sign');
+
+const videoList = [video1, video2, video3]
+
+videoList.forEach(function(video){
+    video.addEventListener('mouseover' , function(){
+        video.play()
+        hoverSign.classList.add("active")
+    })
+
+    video.addEventListener('mouseout' , function(){
+        video.pause()
+        hoverSign.classList.remove("active")
+        video.currentTime = 0; 
+    })
+})
