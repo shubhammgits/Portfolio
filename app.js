b585adf0e6a01449914a54e08f35ee1be9dfcf45
shubhammgits <shubhamm18.work@gmail.com>
2025-06-20 11:34:40 +0530

    update

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const video2 = document.getElementById('projectvideo2');
 const video3 = document.getElementById('projectvideo3');
 const hoverSign = document.querySelector('.hover-sign');
 
+//sidebar
+
+const sidebar = document.querySelector('.sidebar')
+const menu = document.querySelector('.menu-icon')
+
 const videoList = [video1, video2, video3]
 
 videoList.forEach(function(video){
@@ -14,4 +19,9 @@ videoList.forEach(function(video){
         video.pause()
         video.currentTime = 0; 
     })
+})
+
+
+menu.addEventListener("click",  function(){
+    sidebar.classList.add("open-sidebar")
 })
